1c27b032084da67bfe804f0c3c8541cca4141588
aditya sharma <adityasharma42072@gmail.com>
2024-04-01 21:19:32 +0530

    initial commmit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="testing bg-blue-700">
+        <div className='px-2 m-1'>hello there</div>
+      </div>
+    </>
+
+  );
+}
+
+export default App;
